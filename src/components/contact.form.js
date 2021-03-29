02864d5958a9c7bdd2e17dd4700ec28be9ab4122
Manuel Duarte <carlosd99m@gmail.com>
2021-03-29 01:17:58 -0600

    formulario de contacto :art:

---
+++ src/components/contact.form.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default () => (
+  <form className="mt-16 text-center">
+    <label
+      for="contact-content"
+      className="block text-gray-700 text-sm font-bold mb-2"
+    >
+      Cuentame de esa idea que tienes en mente, para que la volvamos realidad:{" "}
+    </label>
+
+    <div className="flex shadow rounded bg-white p-2">
+      <textarea
+        id="contact-content"
+        name="contact-content"
+        className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
+      ></textarea>
+      <button>Enviar</button>
+    </div>
+  </form>
+)
--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,32 @@
 import React from "react"
 import illustration from "../img/undraw_developer_activity_bv83.svg"
+import Form from "../components/contact.form"
 
 export default () => (
   <header className="bg-gray-300">
-    <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
-      <div className="flex-1">
-        <h1 className="font-bold text-purple-700 text-6xl">
-          ¡Hola! Soy Manuel
-        </h1>
-        <p className="text-xl font-light">
-          Desarrolador de aplicaciones web y movil
-        </p>
+    <div className="container mx-auto p-12 max-w-4xl ">
+
+        <div className="flex justify-center items-center">
+           
+            <div className="flex-1">
+                <h1 className="font-bold text-purple-700 text-6xl">
+                ¡Hola! Soy Manuel
+                </h1>
+                <p className="text-xl font-light">
+                Desarrolador de aplicaciones web y movil
+                </p>
+            </div>
+            <img
+                src={illustration}
+                alt="Un hombre haciendo la actividad de programar"
+                style={{ height: "300px" }}
+            ></img>
+        </div>
+
+
+      <div>
+        <Form />
       </div>
-      <img
-        src={illustration}
-        alt="Un hombre haciendo la actividad de programar"
-        style={{ height: "300px" }}
-      ></img>
     </div>
   </header>
 )
