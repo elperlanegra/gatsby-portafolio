b870f1322bbaeada98f34289dbb9cf3e777da774
Manuel Duarte <carlosd99m@gmail.com>
2021-03-29 00:20:11 -0600

    componente encabezado :art:

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => (
+    <header className="bg-gray-300">
+        <div className="container mx-auto p-12 max-w-6xl flex justify-center items-center">
+            <div className="flex-1">
+                <h1>Hola Soy Manuel</h1>
+                <p>Soy desarrollador movil  </p>
+            </div>
+            <img></img>
+        </div>
+    </header>
+)
