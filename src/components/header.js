0fc9beca6c62f6fa616d67f74659d3d87bf2aad8
Manuel Duarte <carlosd99m@gmail.com>
2021-03-29 00:59:14 -0600

    agregando imagenes :art:

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React from "react"
+import illustration from "../img/undraw_developer_activity_bv83.svg"
 
 export default () => (
-    <header className="bg-gray-300">
-        <div className="container mx-auto p-12 max-w-6xl flex justify-center items-center">
-            <div className="flex-1">
-                <h1>Hola Soy Manuel</h1>
-                <p>Soy desarrollador movil  </p>
-            </div>
-            <img></img>
-        </div>
-    </header>
+  <header className="bg-gray-300">
+    <div className="container mx-auto p-12 max-w-4xl flex justify-center items-center">
+      <div className="flex-1">
+        <h1 className="font-bold text-purple-700 text-6xl">
+          ¡Hola! Soy Manuel
+        </h1>
+        <p className="text-xl font-light">
+          Desarrolador de aplicaciones web y movil
+        </p>
+      </div>
+      <img
+        src={illustration}
+        alt="Un hombre haciendo la actividad de programar"
+        style={{ height: "300px" }}
+      ></img>
+    </div>
+  </header>
 )
