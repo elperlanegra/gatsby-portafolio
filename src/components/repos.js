f6f072af73f1ab6036affcc498bc2d5d0e425dcb
Manuel Duarte <carlosd99m@gmail.com>
2021-03-30 23:54:42 -0600

    Enlistar repositorios de GitHub

--- src/components/repos.js
+++ src/components/repos.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import repos from '../data/repos'
+import repos from '../data/repos';
+import Repo from './repo';
+
 
 export default () => (
 
-    <div></div>
+    <div className="max-w-4xl mx-auto">
+        <header className="text-center">
+            <h2 className="text-3xl font-bold">Mi trabajo en open source</h2>
+            <p>Colaboración y contribución de código</p>
+        </header>
+
+        <ul className="repos-list">
+            {
+                repos.map((repo) => {
+                    return <Repo repo={repo} key={repo.id} />
+                })
+            }
+        </ul>
+
+    </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import Header from "../components/header"
 import Repos from "../components/repos"
 
-
 export default function Home() {
   return <div >
     <Header />
